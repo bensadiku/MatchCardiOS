6240aba32f4e3665d75d670b1c698341769847e9
behxhet sadiku <bensadiku65@gmail.com>
2019-09-15 12:28:37 -0700

    Added delegates and protocols

--- MatchCardiOS/ViewController.swift
+++ MatchCardiOS/ViewController.swift
@@ -8,21 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var model = CardModel()
     var cardArray  = [Card]()
     
     
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+            
         //Gets the generated card array
         cardArray = model.getCards()
         
+        //assign the view controller
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    // implementing required UICollectionView protocol methods
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
     }
 
-
+    //called when user taps the cell on the grid
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
 
